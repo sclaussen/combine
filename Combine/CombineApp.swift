b733326740349dc002216c2fe37e1de5b6731cdd
Shane Claussen <sclaussen@yahoo.com>
2022-04-11 09:40:47 -0700

    Initial Commit

---
+++ Combine/CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineApp.swift
+//  Combine
+//
+//  Created by Shane Claussen on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
